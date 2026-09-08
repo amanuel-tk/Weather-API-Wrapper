f04cf5ffedeccca1403587819bd46ed3aaa34e8e
Amanuel <amiiko1927@gmail.com>
2026-09-08 23:54:43 +0300

    added query

--- internal/handler/weather.go
+++ internal/handler/weather.go
@@ -1,12 +1,23 @@
 package handler
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 )
 
 func GetWeather(w http.ResponseWriter, r *http.Request) {
+	var valueDecode interface{}
 
-	fmt.Fprint(w, "Current weather information")
+	fmt.Println("Request received", r.URL.Query())
+
+	err := json.NewDecoder(r.Body).Decode(&valueDecode)
+
+	if err != nil {
+		errors.New("Something went wrong")
+	}
+
+	fmt.Println(valueDecode)
 
 }
