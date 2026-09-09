9f85435741f034a6d9d2606f72d2079a13e424cf
Amanuel <amiiko1927@gmail.com>
2026-09-09 23:56:20 +0300

    making the api request

--- internal/handler/weather.go
+++ internal/handler/weather.go
@@ -17,4 +17,13 @@ func GetWeather(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(city)
 
+	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis%20ababa?unitGroup=us&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
+	if err != nil {
+		errors.New("Something went wrong")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message:failed to fetch weather data"))
+		return
+	}
+	defer res.Body.Close()
+
 }
