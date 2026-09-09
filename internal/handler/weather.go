47f6d24cb56e25003a24b5748e56034288b282e8
Amanuel <amiiko1927@gmail.com>
2026-09-09 23:59:20 +0300

    error removed

--- internal/handler/weather.go
+++ internal/handler/weather.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
@@ -10,7 +9,7 @@ func GetWeather(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 
 	if city == "" {
-		errors.New("Something went wrong")
+
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("message:city is needed"))
 	}
@@ -19,7 +18,7 @@ func GetWeather(w http.ResponseWriter, r *http.Request) {
 
 	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis%20ababa?unitGroup=us&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
 	if err != nil {
-		errors.New("Something went wrong")
+
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("message:failed to fetch weather data"))
 		return
