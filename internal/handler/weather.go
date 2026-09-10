91ff470efa2d4df348b4e7573eea0c06f8658a15
Amanuel <amiiko1927@gmail.com>
2026-09-10 23:21:49 +0300

    found the correct data

--- internal/handler/weather.go
+++ internal/handler/weather.go
@@ -1,10 +1,31 @@
 package handler
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 )
+
+type WeatherResponse struct {
+	Location                 string  `json:"location"`
+	Timezone                 string  `json:"timezone"`
+	Temperature              float64 `json:"temperature"`
+	FeelsLike                float64 `json:"feels_like"`
+	Condition                string  `json:"condition"`
+	Humidity                 float64 `json:"humidity"`
+	WindSpeed                float64 `json:"wind_speed"`
+	WindDirection            float64 `json:"wind_direction"`
+	Precipitation            float64 `json:"precipitation"`
+	PrecipitationProbability float64 `json:"precipitation_probability"`
+	CloudCover               float64 `json:"cloud_cover"`
+	Visibility               float64 `json:"visibility"`
+	Pressure                 float64 `json:"pressure"`
+	UVIndex                  float64 `json:"uv_index"`
+	Sunrise                  string  `json:"sunrise"`
+	Sunset                   string  `json:"sunset"`
+	Icon                     string  `json:"icon"`
+}
 
 func GetWeather(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
@@ -17,7 +38,7 @@ func GetWeather(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(city)
 
-	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis%20ababa?unitGroup=us&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
+	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis%20ababa?unitGroup=us&include=current&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
 	if err != nil {
 		errors.New("Something went wrong")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -26,6 +47,18 @@ func GetWeather(w http.ResponseWriter, r *http.Request) {
 	}
 	defer res.Body.Close()
 
-	fmt.Println(res.Body)
+	var newData any
+
+	err = json.NewDecoder(res.Body).Decode(&newData)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+	}
+
+	pretty, err := json.MarshalIndent(newData, "", " ")
+
+	fmt.Println(string(pretty))
+
+	w.WriteHeader(http.StatusFound)
+	w.Write([]byte(string(pretty)))
 
 }
