57ea6ab9503e6acafd6a3c237f4e20a86fd895aa
Amanuel <amiiko1927@gmail.com>
2026-09-10 23:26:23 +0300

    separeting api call and making stuck

--- internal/client/weather_api.go
+++ internal/client/weather_api.go
@@ -1,5 +1,45 @@
 package client
 
-func GetWeather() string {
-	return "Current weather information"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type WeatherResponse struct {
+	Location                 string  `json:"location"`
+	Timezone                 string  `json:"timezone"`
+	Temperature              float64 `json:"temperature"`
+	FeelsLike                float64 `json:"feels_like"`
+	Condition                string  `json:"condition"`
+	Humidity                 float64 `json:"humidity"`
+	WindSpeed                float64 `json:"wind_speed"`
+	WindDirection            float64 `json:"wind_direction"`
+	Precipitation            float64 `json:"precipitation"`
+	PrecipitationProbability float64 `json:"precipitation_probability"`
+	CloudCover               float64 `json:"cloud_cover"`
+	Visibility               float64 `json:"visibility"`
+	Pressure                 float64 `json:"pressure"`
+	UVIndex                  float64 `json:"uv_index"`
+	Sunrise                  string  `json:"sunrise"`
+	Sunset                   string  `json:"sunset"`
+	Icon                     string  `json:"icon"`
+}
+
+func GetWeather(city string) (WeatherResponse, error) {
+	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=us&include=current&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
+	if err != nil {
+		return WeatherResponse{}, errors.New("Something went wrong")
+	}
+	defer res.Body.Close()
+
+	var data WeatherResponse
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return WeatherResponse{}, errors.New("Error decoding JSON")
+	}
+
+	return data, nil
+
 }
--- internal/handler/weather.go
+++ internal/handler/weather.go
@@ -1,64 +1,19 @@
 package handler
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 )
-
-type WeatherResponse struct {
-	Location                 string  `json:"location"`
-	Timezone                 string  `json:"timezone"`
-	Temperature              float64 `json:"temperature"`
-	FeelsLike                float64 `json:"feels_like"`
-	Condition                string  `json:"condition"`
-	Humidity                 float64 `json:"humidity"`
-	WindSpeed                float64 `json:"wind_speed"`
-	WindDirection            float64 `json:"wind_direction"`
-	Precipitation            float64 `json:"precipitation"`
-	PrecipitationProbability float64 `json:"precipitation_probability"`
-	CloudCover               float64 `json:"cloud_cover"`
-	Visibility               float64 `json:"visibility"`
-	Pressure                 float64 `json:"pressure"`
-	UVIndex                  float64 `json:"uv_index"`
-	Sunrise                  string  `json:"sunrise"`
-	Sunset                   string  `json:"sunset"`
-	Icon                     string  `json:"icon"`
-}
 
 func GetWeather(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 
 	if city == "" {
-		errors.New("Something went wrong")
+
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("message:city is needed"))
 	}
 
 	fmt.Println(city)
-
-	res, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/addis%20ababa?unitGroup=us&include=current&key=EN3UGBJT66YSWVX4HR3MT5KDU&contentType=json")
-	if err != nil {
-		errors.New("Something went wrong")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("message:failed to fetch weather data"))
-		return
-	}
-	defer res.Body.Close()
-
-	var newData any
-
-	err = json.NewDecoder(res.Body).Decode(&newData)
-	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-	}
-
-	pretty, err := json.MarshalIndent(newData, "", " ")
-
-	fmt.Println(string(pretty))
-
-	w.WriteHeader(http.StatusFound)
-	w.Write([]byte(string(pretty)))
 
 }
