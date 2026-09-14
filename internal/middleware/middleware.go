67efa3fbb46c5d4d11c2b218de85d172bfec2afc
Amanuel <amiiko1927@gmail.com>
2026-09-14 16:29:36 +0300

    added middle ware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net"
+	"net/http"
+
+	"github.com/amanuel-tk/weather-api-wrapper/internal/ratelimiter"
+)
+
+type Middleware struct {
+	RateLimiter *ratelimiter.RateLimiterIP
+}
+
+func (i *Middleware) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Invalid client address", http.StatusBadRequest)
+			return
+		}
+
+		limiter := i.RateLimiter.GetLimiter(ip)
+
+		if !limiter.Allow() {
+			http.Error(w, "Too many request", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
